fe1bdbfd92b8aacde34d3c5a247790218519423e
Paul Wild <paulwild@gmail.com>
2019-12-04 23:29:12 +0000

    so many tasty sequence methods

--- AdventOfCode2019.Tests/Days4Tests.fs
+++ AdventOfCode2019.Tests/Days4Tests.fs
@@ -4,36 +4,6 @@ open Xunit
 open AdventOfCode2019.Day4
 
 [<Fact>]
-let ``Example1`` () =
-    let input = 111111
-    let expected = true
-    let actual = processNumber input
-    Assert.Equal(expected, actual) 
-
-[<Fact>]
-let ``Example2`` () =
-    let input = 223450
-    let expected = false
-    let actual = processNumber input
-    Assert.Equal(expected, actual) 
-
-[<Fact>]
-let ``Example3`` () =
-    let input = 123789
-    let expected = false
-    let actual = processNumber input
-    Assert.Equal(expected, actual) 
-
-    
-[<Fact>]
-let ``HasEven`` () =
-    let input = "223" |> Seq.toList
-    let expected = true
-    let actual = hasSingleDoubleDigit Single input
-    Assert.Equal(expected, actual) 
-    
-
-[<Fact>]
 let ``Part1`` () =
     let startNumber = 171309
     let endNumber = 643603

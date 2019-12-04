f257ed2547e06e5de2814a3e1a2b5fc12fc4aab6
Paul Wild <paul.wild@open-money.co.uk>
2019-12-04 18:14:26 +0000

    Day-4

---
+++ AdventOfCode2019.Tests/Days4Tests.fs
@@ -0,0 +1,51 @@
+﻿module Days4Tests
+
+open Xunit
+open AdventOfCode2019.Day4
+
+[<Fact>]
+let ``Example1`` () =
+    let input = 111111
+    let expected = true
+    let actual = processNumber input
+    Assert.Equal(expected, actual) 
+
+[<Fact>]
+let ``Example2`` () =
+    let input = 223450
+    let expected = false
+    let actual = processNumber input
+    Assert.Equal(expected, actual) 
+
+[<Fact>]
+let ``Example3`` () =
+    let input = 123789
+    let expected = false
+    let actual = processNumber input
+    Assert.Equal(expected, actual) 
+
+    
+[<Fact>]
+let ``HasEven`` () =
+    let input = "223" |> Seq.toList
+    let expected = true
+    let actual = hasSingleDoubleDigit Single input
+    Assert.Equal(expected, actual) 
+    
+
+[<Fact>]
+let ``Part1`` () =
+    let startNumber = 171309
+    let endNumber = 643603
+    let expected = 1625
+    let actual = Part1 startNumber endNumber
+    Assert.Equal(expected, actual) 
+
+[<Fact>]
+let ``Part2`` () =
+    let startNumber = 171309
+    let endNumber = 643603
+    let expected = 1111
+    let actual = Part2 startNumber endNumber
+    Assert.Equal(expected, actual) 
+    
---
+++ AdventOfCode2019/Day4.fs
@@ -0,0 +1,54 @@
+﻿namespace AdventOfCode2019
+
+module Day4 =
+    let isDoubleDigit first second =
+        if first = second then true else false
+
+    let isIncreasing first second =
+        if first <= second then true else false
+
+    let oddNumber number = number % 2 <> 0 && number > 0
+    
+    type digitType = Single | Double | Multi | ParDouble
+
+    let isSingleDoubleDigit first second prev =
+        let dd = if first = second then true else false
+
+        match prev with 
+        | Single -> if dd then ParDouble else Single
+        | ParDouble -> if dd then Multi else Double
+        | Double -> if dd then Multi else Double
+        | Multi -> if dd then Multi else if not dd then Single else Double
+
+    let rec hasSingleDoubleDigit currentDigit chars =       
+        match chars with 
+        | fst :: snd :: rest -> 
+            let digit = isSingleDoubleDigit fst snd currentDigit 
+            if digit = Double then true else hasSingleDoubleDigit digit (snd :: rest)  
+        | [_] | [] -> if currentDigit = ParDouble then true else false
+              
+    let rec hasIncreasingNumbers chars =      
+        match chars with 
+            | fst :: snd :: rest -> if isIncreasing fst snd then hasIncreasingNumbers (snd :: rest) else false 
+            | [_] | [] -> true
+
+    let rec hasDoubleDigit chars =      
+        match chars with 
+            | fst :: snd :: rest -> if isDoubleDigit fst snd then true else hasDoubleDigit (snd :: rest) 
+            | [_] | [] -> false
+
+    let processNumber (number: int) =
+        let chars = number.ToString() |> Seq.toList
+        chars |> hasIncreasingNumbers && chars |> hasDoubleDigit 
+
+    let processPart2 (number: int) =
+        let chars = number.ToString() |> Seq.toList
+        chars |> hasIncreasingNumbers && chars |> hasDoubleDigit && chars |> hasSingleDoubleDigit Single
+         
+    let Part1 start stop = 
+        let result = [start..stop] |> Seq.filter processNumber
+        result|> Seq.length
+
+    let Part2 start stop = 
+        let result = [start..stop] |> Seq.filter processPart2 |> Seq.toList
+        result |> Seq.length

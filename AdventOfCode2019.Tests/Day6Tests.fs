05b172636f6f2a2d31e28f9d768ecb11ba88991e
Paul Wild <paul.wild@open-money.co.uk>
2019-12-06 19:03:10 +0000

    Day 6

---
+++ AdventOfCode2019.Tests/Day6Tests.fs
@@ -0,0 +1,30 @@
+﻿module Day6Tests
+
+open Xunit
+open AdventOfCode2019.Day6
+open System.IO
+
+let input = "COM)B B)C C)D D)E E)F B)G G)H D)I E)J J)K K)L"
+
+let innerSplit (str: string) = 
+    let split = str.Split(')')
+    (split.[1],split.[0])
+
+[<Fact>]
+let ``Example1`` () =
+    let resultList = input.Split(" ") |> Array.toList |> List.map innerSplit |> Map.ofList
+    let result = countParents resultList 
+    Assert.Equal(42, result)
+
+[<Fact>]
+let ``Part1`` () =
+    let resultList = File.ReadAllLines(@"Input/Day6.txt") |> Array.toList |> List.map innerSplit |> Map.ofList
+    let result = countParents resultList 
+    Assert.Equal(249308, result)
+ 
+[<Fact>]
+let ``Part2`` () =
+    let resultList = File.ReadAllLines(@"Input/Day6.txt") |> Array.toList |> List.map innerSplit |> Map.ofList
+    let result = part2 resultList 
+    Assert.Equal(349, result)
+        
---
+++ AdventOfCode2019/Day6.fs
@@ -0,0 +1,30 @@
+﻿namespace AdventOfCode2019
+
+module Day6 =
+    type Tree = Map<string,string>
+
+    let countParent (map: Tree) str =
+        let rec countInt str tally =
+            match map.TryFind str with 
+            | Some x ->  countInt x (tally + 1)
+            | None -> tally
+        countInt str 0 
+    
+    let countParents (map: Tree) =
+        map |> Map.toSeq |> Seq.map fst |> Seq.fold (fun acc el -> acc + countParent map el) 0 
+
+    let getPath (map: Tree) str =
+        let rec countInt str path =
+            match map.TryFind str with 
+            | Some x ->  countInt x (x :: path)
+            | None -> path
+        countInt str []
+
+    let part2 (map: Tree) =
+        let you = getPath map "YOU" |> set
+        let san = getPath map "SAN" |> set
+
+        let res = Set.intersect you san |> Set.count
+        (you |> Set.count) + (san |> Set.count) - (res *2)
+
+

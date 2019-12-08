328bfa003ea5480c27f91308cb27225f0f4cc49d
Paul Wild <paulwild@gmail.com>
2019-12-08 16:50:57 +0000

    Day-7 Done at last

--- AdventOfCode2019/Day7.fs
+++ AdventOfCode2019/Day7.fs
@@ -2,7 +2,6 @@ namespace AdventOfCode2019
 
 open Day2
 
-
 module Day7 =
     let permutations (l: int list) = 
         let rec permInt (blah:int list*int list) =
@@ -38,10 +37,30 @@ module Day7 =
         let phases = [4;3;2;1;0] 
         let perm = permutations phases
         perm |> List.map (fun x -> runForDay7 input (Some 0) x) |> List.max
-//
-//        
-//    let runForPart2 input =
-//    
-//        let phases = [9;8;7;6;5] 
-//        let perm = permutations phases
-//        perm |> List.map (fun x -> run x input 0) |> List.max
+        
+    let rec feedback a1 a2 a3 a4 a5 input =
+        let amp1 = Processor {a1 with Input = input }
+        let amp2 = Processor {a2 with Input = amp1.Output } 
+        let amp3 = Processor {a3 with Input = amp2.Output }
+        let amp4 = Processor {a4 with Input = amp3.Output }
+        let amp5 = Processor {a5 with Input = amp4.Output }
+        
+        match amp5.State with
+        | Running | Input -> feedback amp1 amp2 amp3 amp4 amp5 amp5.Output
+        | Halted -> amp5.Output
+
+    let runForPart2 (input: string) =
+        let phases = [9;8;7;6;5]
+        let perm = permutations phases
+        let map = input.Split ','
+                    |> Seq.map int
+                    |> Seq.mapi (fun idx value -> (idx, value))
+                    |> Map.ofSeq
+        let data = { IntCodes=map; Input=(Some 0); CurrentIndex=0; Output=None; State=Running}  
+        perm |> List.map (fun x -> feedback
+                                    (Processor {data with Input=Some x.[0]})
+                                    (Processor {data with Input=Some x.[1]})
+                                    (Processor {data with Input=Some x.[2]})
+                                    (Processor {data with Input=Some x.[3]})
+                                    (Processor {data with Input=Some x.[4]})
+                                    (Some 0)) |> List.max

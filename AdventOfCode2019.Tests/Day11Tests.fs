5b49a890ab8be435efd3638139f5a05b5c97c065
Paul Wild <paul.wild@open-money.co.uk>
2019-12-11 12:54:26 +0000

    Day 11 - First gogit add .!
    mend

--- AdventOfCode2019.Tests/Day11Tests.fs
+++ AdventOfCode2019.Tests/Day11Tests.fs
@@ -7,7 +7,7 @@ open System.IO
 [<Fact>]
 let ``Part1`` () =
     let input = File.ReadAllText(@"Input/Day11.txt")
-    let expected = 5
+    let expected = 2268
     
     let actual = part1 input 
     Assert.Equal(expected, Map.count actual) 

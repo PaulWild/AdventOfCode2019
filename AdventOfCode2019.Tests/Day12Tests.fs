906efab215b6d262915d1acc29961c171464fcf0
Paul Wild <paul.wild@open-money.co.uk>
2019-12-12 12:52:24 +0000

    Day 12 Part 1

---
+++ AdventOfCode2019.Tests/Day12Tests.fs
@@ -0,0 +1,23 @@
+﻿module Day12Tests
+
+open Xunit
+open System.IO
+open AdventOfCode2019.Day12
+
+
+
+let day12Input = [InitPlanet {X= 3; Y= -6; Z= 6}; InitPlanet {X=10; Y= 7; Z= -9}; InitPlanet {X= -3; Y= -7; Z= 9}; InitPlanet {X= -8; Y= 0; Z= 4}]
+    
+[<Fact>]
+let ``Example2`` () =
+    let input = [InitPlanet {X= -1; Y= 0; Z= 2}; InitPlanet {X=2; Y= -10; Z= -7}; InitPlanet {X= 4; Y= -8; Z= 8}; InitPlanet {X= 3; Y= 5; Z= -1}]
+    let result = part1 input 10
+    let expected = 179
+    Assert.Equal(expected, result) 
+
+[<Fact>]
+let ``Part1`` () =
+    let result = part1 day12Input 1000
+    let expected = 179
+    Assert.Equal(expected, result) 
+
---
+++ AdventOfCode2019/Day12.fs
@@ -0,0 +1,36 @@
+﻿namespace AdventOfCode2019
+
+open System
+
+
+
+module Day12 = 
+    type Pos = { X: int; Y: int;  Z:int }
+    type Velocity ={ vX: int; vY: int;  vZ:int }
+    
+    type Planet = { Position: Pos; Velocity: Velocity  }
+    
+    let InitPlanet pos =
+        { Position = pos; Velocity = {vX=0; vY=0; vZ=0} }
+        
+    let calculateNewVelocity y x =
+        if y > x then 1 else
+        if y < x then -1 else
+        0
+    
+    let calculateVelocity (planet: Planet) (planets: Planet list) =               
+        planets |> List.fold (fun agg curr -> {agg with X=agg.X + calculateNewVelocity curr.Position.X planet.Position.X; Y=agg.Y + calculateNewVelocity curr.Position.Y planet.Position.Y; Z=agg.Z + calculateNewVelocity curr.Position.Z planet.Position.Z}) {X=0; Y=0; Z=0}
+    
+    let calculateVelocities (planets: Planet List) =
+        planets |> List.map (fun x ->
+            let y = calculateVelocity x planets
+            {x with  Velocity = {vX = x.Velocity.vX + y.X; vY =  x.Velocity.vY + y.Y; vZ =  x.Velocity.vZ + y.Z;  }})
+        |> List.map (fun x -> {x with Position = {X = x.Position.X + x.Velocity.vX; Y = x.Position.Y + x.Velocity.vY; Z = x.Position.Z + x.Velocity.vZ; }})
+        
+
+    let rec runFor planets runCount =
+        if runCount = 0 then planets else
+            runFor (calculateVelocities planets) (runCount-1)
+            
+    let part1 planets runCount =
+        runFor planets runCount |> List.map (fun x -> (Math.Abs(x.Position.X) + Math.Abs(x.Position.Y) + Math.Abs(x.Position.Z)) * (Math.Abs(x.Velocity.vX) + Math.Abs(x.Velocity.vY) + Math.Abs(x.Velocity.vZ))) |> List.sum

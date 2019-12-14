ea35f7a72522bf76931f9ca0e01dc8b2d29b1324
Paul Wild <paul.wild@open-money.co.uk>
2019-12-14 00:36:40 +0000

    day 12 with help from the interwebs

--- AdventOfCode2019.Tests/Day12Tests.fs
+++ AdventOfCode2019.Tests/Day12Tests.fs
@@ -16,14 +16,45 @@ let ``Example2`` () =
     Assert.Equal(expected, result) 
 
 [<Fact>]
+let ``Example5`` () =
+    let input = [InitPlanet {X= -8; Y= -10; Z= 0}; InitPlanet {X=5; Y= 5; Z= 10}; InitPlanet {X= 2; Y= -7; Z= 3}; InitPlanet {X= 9; Y= -8; Z= -3}]
+    let result = part1 input 100
+    let expected = 1940
+    Assert.Equal(expected, result) 
+
+
+[<Fact>]
 let ``Part1`` () =
     let result = part1 day12Input 1000
     let expected = 6849
     Assert.Equal(expected, result) 
 
 
+
+
 [<Fact>]
-let ``lcdTest1`` () =
-    let result = lcd [33; 27]
-    let expected = 3
+let ``Part2`` () =
+    let result = part2 day12Input
+    let expected = 356658899375688L
+    Assert.Equal(expected, result) 
+
+[<Fact>]
+let ``Example3`` () =
+    let input =[InitPlanet {X= -1; Y= 0; Z= 2}; InitPlanet {X=2; Y= -10; Z= -7}; InitPlanet {X= 4; Y= -8; Z= 8}; InitPlanet {X= 3; Y= 5; Z= -1}]
+    let result = part2 input 
+    let expected = int64 2772
+    Assert.Equal(expected, result) 
+
+
+[<Fact>]
+let ``Example6`` () =
+    let input = [InitPlanet {X= -8; Y= -10; Z= 0}; InitPlanet {X=5; Y= 5; Z= 10}; InitPlanet {X= 2; Y= -7; Z= 3}; InitPlanet {X= 9; Y= -8; Z= -3}]
+    let result = part2 input 
+    let expected = 4686774924L
+    Assert.Equal(expected, result) 
+
+[<Fact>]
+let ``tupleEqual`` () =
+    let result = (1,2,2) = (1,2,2)
+    let expected = true
     Assert.Equal(expected, result) 
--- AdventOfCode2019/Day12.fs
+++ AdventOfCode2019/Day12.fs
@@ -8,7 +8,8 @@ module Day12 =
     type Pos = { X: int; Y: int;  Z:int }
     type Velocity ={ vX: int; vY: int;  vZ:int }
     type Planet = { Position: Pos; Velocity: Velocity  }
-    
+    type LoopResult = { StartPos: (int*int*int*int*int*int*int*int); HasLooped:bool; count:int; }
+     
     let InitPlanet pos =
         { Position = pos; Velocity = {vX=0; vY=0; vZ=0} }
         
@@ -33,7 +34,38 @@ module Day12 =
     let rec runFor planets runCount =
         if runCount = 0 then planets else
             runFor (calculateVelocities planets) (runCount-1)
-            
+
+    let getPos planet =
+        (planet.Position.X, planet.Position.Y, planet.Position.Z,planet.Velocity.vX,planet.Velocity.vY,planet.Velocity.vZ)
+        
+    let eurgh (planets: Planet list) xyz =
+        let (x,y,z) = xyz
+        let (X1,Y1,Z1,vX1,vY1,vZ1) = getPos (planets.[0])
+        let (X2,Y2,Z2,vX2,vY2,vZ2) = getPos (planets.[1])
+        let (X3,Y3,Z3,vX3,vY3,vZ3) = getPos (planets.[2])
+        let (X4,Y4,Z4,vX4,vY4,vZ4) = getPos (planets.[3])
+        let xn = if (not x.HasLooped) && x.StartPos = (X1,vX1,X2,vX2,X3,vX3,X4,vX4) then {x with HasLooped = true; count = x.count+1}
+                    else if not x.HasLooped then { x with count = x.count+1;}
+                    else x
+        let yn = if (not y.HasLooped) && y.StartPos = (Y1,vY1,Y2,vY2,Y3,vY3,Y4,vY4) then {y with HasLooped = true; count = y.count+1}
+                    else if not y.HasLooped then { y with count = y.count+1;}
+                    else y
+        let zn = if (not z.HasLooped) && z.StartPos = (Z1,vZ1,Z2,vZ2,Z3,vZ3,Z4,vZ4) then {z with HasLooped = true; count = z.count+1}
+                    else if not z.HasLooped then { z with count = z.count+1;}
+                    else z
+        (xn,yn,zn)
+        
+    //fuck it there is four not going to make it generic, just want it done
+    let rec runUntilLooped planets p1  =
+        let pl = calculateVelocities planets
+        
+        let (p1nx,p1ny,p1nz) = eurgh pl p1
+        
+        if (p1nx.HasLooped && p1ny.HasLooped && p1nz.HasLooped) then
+            (p1nx,p1ny,p1nz)
+        else
+            runUntilLooped pl (p1nx,p1ny,p1nz)
+        
     let part1 planets runCount =
         runFor planets runCount
         |> List.map (fun x ->
@@ -42,12 +74,28 @@ module Day12 =
         |> List.sum
         
         
-    let rec gcd fst  snd =
+    let rec gcd (fst: int64)  (snd: int64) =
         if fst < snd then gcd snd fst
-        else if fst % snd = 0 then snd
+        else if fst % snd = (int64 0) then snd
         else gcd snd (fst % snd)
         
-    let lcd (numbers: int list)=
-        let g = numbers |> List.reduce (fun agg curr -> gcd agg curr)
-        let m = numbers |> List.reduce (fun agg curr -> agg * curr) 
-        m/g
+    let lcd (numbers: int64 list)=
+        let lcdInt (n1:int64) (n2: int64) =
+            (n1*n2)/(gcd n1 n2)
+        numbers |> List.reduce (fun agg curr -> lcdInt agg curr)
+        
+    
+    let initResult (planets: Planet list) =
+        
+        let (X1,Y1,Z1,vX1,vY1,vZ1) = getPos (planets.[0])
+        let (X2,Y2,Z2,vX2,vY2,vZ2) = getPos (planets.[1])
+        let (X3,Y3,Z3,vX3,vY3,vZ3) = getPos (planets.[2])
+        let (X4,Y4,Z4,vX4,vY4,vZ4) = getPos (planets.[3])
+        ({ StartPos=(X1,vX1,X2,vX2,X3,vX3,X4,vX4); HasLooped=false; count=0; },
+         { StartPos=(Y1,vY1,Y2,vY2,Y3,vY3,Y4,vY4); HasLooped=false; count=0; },
+         { StartPos=(Z1,vZ1,Z2,vZ2,Z3,vZ3,Z4,vZ4); HasLooped=false; count=0; })
+        
+    let part2 planets =
+        let ((p1x,p1y,p1z)) = runUntilLooped planets (initResult planets)
+        lcd [int64 p1x.count;int64 p1y.count;int64 p1z.count]
+ 

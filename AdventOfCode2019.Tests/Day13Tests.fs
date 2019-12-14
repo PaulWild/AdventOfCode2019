549ec75b4e4b62ba86f724024680292d7a76b4a7
Paul Wild <paul.wild@open-money.co.uk>
2019-12-14 02:11:29 +0000

    Day 13

---
+++ AdventOfCode2019.Tests/Day13Tests.fs
@@ -0,0 +1,26 @@
+﻿module Day13Tests
+
+open Xunit
+open AdventOfCode2019.Day13
+open System.IO
+open Xunit.Abstractions
+  
+type Day13Tests(output:ITestOutputHelper) =
+
+    [<Fact>]
+    member __.``Part1`` () =
+        let input = File.ReadAllText(@"Input/Day13.txt")
+        let expected = 236
+        
+        let actual = part1 input
+
+        Assert.Equal(expected, actual) 
+
+    [<Fact>]
+    member __.``Part2`` () =
+        let input = File.ReadAllText(@"Input/Day13.txt")
+        let expected = 11040L
+        
+        let actual = part2 input
+
+        Assert.Equal(expected, actual) 
---
+++ AdventOfCode2019/Day13.fs
@@ -0,0 +1,60 @@
+﻿namespace AdventOfCode2019
+
+open System
+open IntCode
+
+module Day13 =
+    type Pos = { X:int64; Y:int64 }
+    
+    type Tile = Empty | Wall | Block | Paddle | Ball | Score of int64
+    
+    type Pixel = {Position: Pos; Tile: Tile}
+    
+    let intToTile num =
+        match num with
+        | 0L -> Empty
+        | 1L -> Wall
+        | 2L -> Block
+        | 3L -> Paddle
+        | 4L -> Ball
+        | x -> Score x 
+    
+    let rec chunk output res =
+        match output with
+        | tile :: y :: x :: xs -> chunk xs (({X=x; Y=y},intToTile tile) :: res)
+        | _ -> res 
+        
+    let part1 input =
+        let map = stringToMap input
+        let data = InitState map None 
+        
+        let output = Processor data
+        chunk output.Output List.empty |> List.filter (fun (pos,x) -> x = Block) |> List.length
+        
+    let rec play input theBoard =
+        let output = Processor input
+        let board = chunk output.Output List.empty 
+
+        let newBoard = board  |> Map |> Map.fold (fun (agg: Map<Pos,Tile>) pos tile -> agg.Add(pos,tile))  theBoard
+
+        let paddle = newBoard |> Map.toList |> List.find (fun (_,x) -> x = Paddle) |> fst
+        let ball = newBoard |> Map.toList |> List.find (fun (_,x) -> x = Ball) |> fst
+        let blockCount = newBoard |> Map.toList |> List.filter (fun (_,x) -> x = Block) |> List.length
+        
+        let newInput = if (ball.X < paddle.X) then {output with Input = Some -1L; Output = List.empty } else
+                       if (ball.X > paddle.X) then {output with Input = Some 1L; Output = List.empty } else
+                       {output with Input = Some 0L;  Output = List.empty }
+  
+        if blockCount = 0 then
+            let score = board |> List.find (fun (_,x) -> match x with | Score(_) -> true | _ -> false) |> snd
+            match score with
+            | Score(x) -> x
+            | _ -> -1L
+        else
+            play newInput newBoard
+        
+
+    let part2 input =
+        let map = (stringToMap input) |> Map.add 0L 2L
+        let data = InitState map None 
+        play data Map.empty
